e985626a898f49db580904e4d6263254b011ee35
Achu Ajesh <achuv37@gmail.com>
2022-05-05 22:29:18 -0400

    Add product model

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,92 +3,101 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  
-  Category.findAll()
-    .then(dbCategoryData => res.json(dbCategoryData))
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
+  try {
+    const categoryData = await Category.findAll({
+      include: [{
+        model: Product,
+      }]
     });
+    if (!categoryData) {
+      res.status(404).json({
+        message: "No categories found"
+      });
+    } res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+    
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  Category.findOne({
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(dbCategoryData => {
-      if (!dbCategoryData) {
-        res.status(404).json({ message: 'No categories found with this id' });
-        return;
-      }
-      res.json(dbCategoryData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-
-});
-
-router.post('/', (req, res) => {
-  // create a new category
-  Category.create({
-    category_name: req.body.category_name,
+  try {
+  const categoryData = await Category.findByPk(req.params.id, {
+    include: [{
+      model: Product,
+    }]
+  });
     
-  })
-    .then(dbCategoryData => res.json(dbCategoryData))
-    .catch(err => {
-      console.log(err);
+  if (!categoryData) {
+        res.status(404).json({ message: 'No categories found with this id' });
+        
+      }
+      res.status(200).json(categoryData);
+    } catch (error) {
       res.status(500).json(err);
-    });
+    }
+  });
+    
 
-});
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+      res.status(200).json(newCategory);
+  } catch (error) {
+    res.status(500).json(err);
+  }
+    
+  });
+    
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-  Category.update(req.body, {
-    where: {
-      id: req.params.id
-    }
-  })
-    .then(dbCategoryData => {
-      if (!dbCategoryData[0]) {
-        res.status(404).json({ message: 'No categories found with this id' });
-        return;
+  try {
+    const updatedCategory = await Category.update({
+    id: req.params.id,
+    category_name: req.body.category_name
+    }, {
+      where: {
+        id: req.params.id
       }
-      res.json(dbCategoryData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+    
+  });
+   if (!updatedCategory[0]) {
+      res.status(404).json({ message: 'No categories found with this id' });
+        
+      }
+      res.status(200).json(updatedCategory);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+    
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
-  Category.destroy({
-    where: {
+  try {
+    const deletedCategory = await Category.destroy(
+    {
+      where: {
       id: req.params.id
     }
-  })
-    .then(dbCategoryData => {
-      if (!dbCategoryData) {
+  });
+
+  if (!deletedCategory) {
         res.status(404).json({ message: 'No categories found with this id' });
         return;
       }
-      res.json(dbCategoryData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+      res.status(200).json(deletedCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+    
 });
 
 module.exports = router;
